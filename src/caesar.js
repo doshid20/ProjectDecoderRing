0e2d372737aa7e70ac66784f68c6be719c09db3e
doshid20 <79118790+doshid20@users.noreply.github.com>
2021-03-25 11:32:04 -0700

    Add logic for caesar

--- src/caesar.js
+++ src/caesar.js
@@ -5,9 +5,74 @@
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
+  const regex = "[a-zA-Z]";
 
+  /**
+   * Helper function to convert lowercase
+   * @param {*} input 
+   * @returns 
+   */
+
+  function convertToLowerCase(input) {
+    const stringWithLowerCase = input.toLowerCase();
+    return stringWithLowerCase;
+  } 
+
+  /**
+   *  Helper function to shift characters
+   * @param {} letter 
+   * @param {*} shift 
+   * @returns 
+   */
+
+  function shiftletter(letter, shift) {
+    let newLetter ="";
+    let currentLetter = letter.charCodeAt(0);
+    let newLetterCode = currentLetter + shift;
+
+    if (newLetterCode < 97) {
+      newLetterCode += 26;
+    } else if (newLetterCode > 122) {
+      newLetterCode -= 26;
+    }
+    newLetter = String.fromCharCode(newLetterCode);
+
+    return newLetter;
+  }
+
+  // function with caesar cipher algorithm
   function caesar(input, shift, encode = true) {
     // your solution code here
+
+    let encryptedMessage = "";
+    const inputInLowercase = convertToLowerCase(input);
+    let messageToBeEncrypted = [...inputInLowercase];
+
+    
+    if( !shift || shift === 0 || shift < -25 || shift > 25 ) {
+      return false;
+    }
+
+    if(encode) {
+
+      messageToBeEncrypted = messageToBeEncrypted.forEach ((char) => {
+        if (!char.match(regex)) {
+          return encryptedMessage += char;
+        }
+         encryptedMessage += shiftletter(char, shift);        
+      })
+
+    } else {
+       messageToBeEncrypted = messageToBeEncrypted.forEach ((char) => {
+        if (!char.match(regex)) {
+          return encryptedMessage += char;
+        }
+         encryptedMessage += shiftletter(char, shift);        
+      })
+    }
+
+    return encryptedMessage;
+
   }
 
   return {
@@ -16,3 +81,4 @@ const caesarModule = (function () {
 })();
 
 module.exports = caesarModule.caesar;
+
