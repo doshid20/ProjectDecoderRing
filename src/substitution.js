228c4eb4c8a3c7b15838210d4f6b00964db9d102
doshid20 <79118790+doshid20@users.noreply.github.com>
2021-03-25 12:29:49 -0700

    add logic for substitution
    add logic for substitution

--- src/substitution.js
+++ src/substitution.js
@@ -6,8 +6,79 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+
+    let result = "";
+    const realAplhabet = "abcdefghijklmnopqrstuvwxyz";
+    let inputInLowerCase = input.toLowerCase();
+
+    /** 
+     * Create array of substitution alphabet
+     * and check if any duplicate 
+     * return false in case of duplicate found
+     */
+
+    function isCharactersInSubstitionAlphabetUnique(alphabet) {
+
+      const arrOfAlphabet = [...alphabet];
+      for (let character of arrOfAlphabet) {
+
+      const isDuplicateFound = arrOfAlphabet.filter((char) => (character === char))
+      
+        if (isDuplicateFound.length > 1) {
+          return false;
+        }
+      return true;
+      }
+    }
+
+    if (!alphabet || !input) {
+      return false;
+    }
+
+    if (alphabet.length != 26) {
+      return false;
+    }
+
+    if (!isCharactersInSubstitionAlphabetUnique(alphabet)) {
+      return false;
+    }
+
+    const encodeAlphaMapping = [...realAplhabet].reduce((alpha, char, index)=>{
+        alpha[char] = alphabet[index];
+        return alpha;
+      },{})
+
+    const decodeAlphaMapping = [...alphabet].reduce((alpha, char, index)=> {
+      alpha[char] = realAplhabet[index];
+      return alpha;
+    }, {}) 
+
+    if(encode) {
+
+      for (let char of inputInLowerCase) {
+        if (char === " ") {
+          result += " ";
+          continue;
+        }
+        result += encodeAlphaMapping[char];
+      }
+
+    } else {
+
+      for (let char of inputInLowerCase) {
+        if (char === " ") {
+          result += " ";
+          continue;
+        }
+        result += decodeAlphaMapping[char];
+      }
+    }
+
+    return result;
+    
   }
 
   return {
@@ -16,3 +87,5 @@ const substitutionModule = (function () {
 })();
 
 module.exports = substitutionModule.substitution;
+
+
